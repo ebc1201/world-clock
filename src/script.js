dc9c100e964957ca9d420385c721be32820b5750
Erica Chambers <53728223+ebc1201@users.noreply.github.com>
2023-02-09 18:55:14 -0600

    Added current location

--- src/script.js
+++ src/script.js
@@ -41,9 +41,11 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-  console.log(cityTime.format("MMMM Do YYYY"));
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `<div class="city" id="sydney">
         <div>

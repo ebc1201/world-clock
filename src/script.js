1dff1ccbab57742166024b18bff5e203a4b9219c
Erica Chambers <53728223+ebc1201@users.noreply.github.com>
2023-02-10 11:58:30 -0600

    Updated script to removed sydney id

--- src/script.js
+++ src/script.js
@@ -47,7 +47,7 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `<div class="city" id="sydney">
+  citiesElement.innerHTML = `<div class="city">
         <div>
         <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
